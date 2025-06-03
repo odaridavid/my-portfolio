7a6f8597f777eaef9824352b131f849820b9c215
David Odari <david.kiribwa@blacklane.com>
2025-06-03 23:15:50 +0200

    put tbd on projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,7 +2,7 @@ export default function Projects() {
     return (
       <section className="max-w-2xl mx-auto mt-20">
         <h2 className="text-2xl font-bold mb-4">Projects</h2>
-        <p>I’m a software engineer who likes clean code, lo-fi beats, and cinematic shots.</p>
+        <p>TBD</p>
       </section>
     );
   }
