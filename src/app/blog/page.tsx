77db8dd0991b8784982f351191449243b05b48d2
David Odari <david.kiribwa@blacklane.com>
2025-06-03 19:45:15 +0200

    Add blog dummy page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,8 @@
+export default function Blog() {
+    return (
+      <section className="max-w-2xl mx-auto mt-20">
+        <h2 className="text-2xl font-bold mb-4">Projects</h2>
+        <p>I’m a software engineer who likes clean code, lo-fi beats, and cinematic shots.</p>
+      </section>
+    );
+  }
